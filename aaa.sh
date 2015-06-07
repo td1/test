883aecced1a7c33ad5030b6e28ddde3bee61cfd4
td1 <hnguye17@uci.edu>
2015-06-07 15:46:00 -0700

    remove update playing.sh

--- aaa.sh
+++ aaa.sh
@@ -2,6 +2,6 @@
 cp picoApp.cpp ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/src/
 cp picoApp.h ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/src/
 cp main.cpp ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/src/
-cp playing.sh ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/bin/
+#cp playing.sh ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/bin/
 chmod 777 ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/src/picoApp.cpp
 cp sample4qr*.png ~/winshare 
