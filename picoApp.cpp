3f5678dc84b77327cd11dd741b268d9a07bf4196
td1 <hnguye17@uci.edu>
2015-06-18 13:52:29 -0700

    enable offset, update with non-overlapping offset values

--- picoApp.cpp
+++ picoApp.cpp
@@ -1637,9 +1637,9 @@ FILE *matp;
     offset4[1][3] = 0; 
     offset4[2][3] = 0; 
 #else
-    offset2[1][3] = 560; 
+    offset2[1][3] = 640; 
     offset3[2][3] = 480; 
-    offset4[1][3] = 560; 
+    offset4[1][3] = 640; 
     offset4[1][3] = 480; 
 #endif
     
