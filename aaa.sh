16e05e99f9daf90d41aa36b98c2ad39b9aeb4db1
td1 <hnguye17@uci.edu>
2015-07-08 23:35:58 -0700

    minor update

--- aaa.sh
+++ aaa.sh
@@ -11,8 +11,8 @@ cp blend1.txt ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/bin/
 cp blend2.txt ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/bin/
 cp blend3.txt ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/bin/
 cp blend4.txt ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/bin/
-cp ./testpattern/testpattern.mp4 ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/bin/data/
+#cp ./testpattern/testpattern.mp4 ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/bin/data/
 cp bbb.sh ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/
-cp playing.sh ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/bin/
+#cp playing.sh ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/bin/
 chmod 777 ~/openFrameworks/addons/ofxOMXPlayer/hung_pixel_dl/src/picoApp.cpp
 cp sample8qr*.png ~/winshare 
