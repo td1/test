438614b1e6b8eb7bf85b8e250bbbcf0fe99f4f73
td1 <hnguye17@uci.edu>
2015-07-12 15:07:32 -0700

    set offset to (560,240)

--- picoApp.h
+++ picoApp.h
@@ -51,8 +51,8 @@ extern "C" {
 #define UPPERLEFT_Y         140 // pixels
 #define CAPTURE_RATE        1	// seconds
 
-#define HOVL                620
-#define VOVL                460
+#define HOVL                560
+#define VOVL                240
 
 #define NUMBER_OF_POINTS    8
 #define NROW                16
